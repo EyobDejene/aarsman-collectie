d80624d881a4ff75e2930fdb056b884588de17c5
eyobdejene <eyob_westerink@hotmail.com>
2020-06-08 13:52:42 +0200

    added experimental page

--- assets/js/aarsman.js
+++ assets/js/aarsman.js
@@ -1,17 +1,50 @@
 let click = "click";
 let hover = "hover";
+let steady = "steady";
+let fast = "fast";
+let unlock = "unlock";
+let spy = "background";
+
 
 function loadSound () {
   createjs.Sound.registerSound("assets/audio/click.mp3", click);
   createjs.Sound.registerSound("assets/audio/hover.mp3", hover);
+  createjs.Sound.registerSound("assets/audio/chest_steady.mp3", steady);
+  createjs.Sound.registerSound("assets/audio/chest_fast.mp3", fast);
+  createjs.Sound.registerSound("assets/audio/reveal.mp3", unlock);
+  createjs.Sound.registerSound("assets/audio/search.mp3", spy);
 }
 
 
-function ClickSound () {
+function ClickSound() {
   createjs.Sound.play(click);
 }
 
-function HoverSound () {
+function HoverSound() {
   createjs.Sound.play(hover);
 }
+
+function AchievementSound() {
+  createjs.Sound.play(unlock);
+}
+
+
+function backgroundMusic() {
+  let background = createjs.Sound.play(spy);
+  background.loop = -1;
+  background.volume = 0.5;
+}
+
+
+
+
+function Chest(state) {
+
+    // let beat = createjs.Sound.play(state);
+    // beat.volume = 0.5 ;
+    // beat.loop = -1;
+    //
+    // console.log(beat);
+}
+
 
